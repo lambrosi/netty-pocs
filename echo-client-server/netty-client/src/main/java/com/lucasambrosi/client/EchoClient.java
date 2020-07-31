e648cb7f29772f664151629ab70718314fcf5ea1
Lucas Ambrosi Derrossi <lucas_ambrosi@yahoo.com.br>
2020-07-31 00:28:23 -0300

    Fix channel class implementation

--- echo-client-server/netty-client/src/main/java/com/lucasambrosi/client/EchoClient.java
+++ echo-client-server/netty-client/src/main/java/com/lucasambrosi/client/EchoClient.java
@@ -6,7 +6,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.net.InetSocketAddress;
 
@@ -26,7 +26,7 @@ public class EchoClient {
         try {
             Bootstrap bootstrap = new Bootstrap()
                     .group(group)
-                    .channel(NioServerSocketChannel.class)
+                    .channel(NioSocketChannel.class)
                     .remoteAddress(new InetSocketAddress(host, port))
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
